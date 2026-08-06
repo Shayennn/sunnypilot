e0d10ef5b2ee40488b23784f2222027d2c3e5939
Phitchawat L. <8080856+Shayennn@users.noreply.github.com>
2026-08-06 15:25:02 +0700

    style(ui): polish driver yaw mode controls

--- openpilot/selfdrive/ui/mici/layouts/settings/device.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/device.py
@@ -165,7 +165,7 @@ class DriverMonitoringYawModeButton(BigMultiToggle):
 
   def __init__(self):
     self._params = ui_state.params
-    super().__init__("driver yaw\nmode", list(self.OPTIONS.values()), select_callback=self._select_mode)
+    super().__init__("Driver Yaw\nMode", list(self.OPTIONS.values()), select_callback=self._select_mode)
     self.refresh()
 
   def _get_label_font_size(self):
